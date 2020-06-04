0f5e354819465b56a64d08f057d1e835cd603f47
peeyusha-tech <peeyusha.patil@gmail.com>
2020-06-04 23:00:50 +0100

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        CountStepsToZero obj= new CountStepsToZero();
+        int step=obj.numberOfSteps(14);
+        System.out.println("Steps:"+step);
+    }
+}
